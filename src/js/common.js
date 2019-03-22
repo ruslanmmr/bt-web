6d441ff6af3b2cd9345c82e1240324c043738e54
ruslan <belozerzevruslan@gmail.com>
2019-03-22 13:49:33 +0200

    added

--- src/js/common.js
+++ src/js/common.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
   nav();
   backgroundSizing();
+  slider();
+  clientsSlider();
 });
 $(window).resize(function () {
   innerWidth = $('body').innerWidth();
+  backgroundSizing();
 });
 var innerWidth = $('body').innerWidth(),
     overlay = $('.overlay');
@@ -47,8 +50,91 @@ function nav() {
 }
 function backgroundSizing() {
   var bgGallery = $('.gallery__background'),
-      bgWidth = $('.gallery__section_controls').width() + 30,
-      containerWidth = $('.gallery__container').width();
-  console.log(bgWidth, innerWidth, (containerWidth / 2))
-  bgGallery.width(bgWidth + ((innerWidth - containerWidth) / 2));
+      bgGalleryWidth = $('.gallery__section_controls').width() + 30,
+      containerGalleryWidth = $('.gallery__container').width();
+  var bgContacts = $('.contacts__background'),
+      bgContactsWidth = $('.contacts__section_content').width() + 30,
+      containerContactsWidth = $('.contacts__container').width();
+
+  bgGallery.width(bgGalleryWidth + ((innerWidth - containerGalleryWidth) / 2));
+  bgContacts.width(bgContactsWidth + ((innerWidth - containerContactsWidth) / 2));
+}
+function slider() {
+  var slider = $('.gallery-slider'),
+      sliderNav = $('.gallery-nav__list');
+  
+  slider.slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    arrows: true,
+    asNavFor: '.gallery-nav__list',
+    draggable: false,
+    speed: 300,
+    swipe: false,
+    infinite: true,
+    nextArrow: '.gallery-controls__next',
+    prevArrow: '.gallery-controls__prev',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });  
+  sliderNav.slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    asNavFor: '.gallery-slider',
+    dots: false,
+    arrows: false,
+    focusOnSelect: true,
+    draggable: false,
+    speed: 0,
+    swipe: false,
+    variableWidth: true,
+  });
+}
+function clientsSlider() {
+  var slider = $('.clients-slider');
+
+  slider.slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: false,
+    arrows: true,
+    draggable: false,
+    speed: 300,
+    swipe: false,
+    infinite: true,
+    nextArrow: '.clients__next',
+    prevArrow: '.clients__prev',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true
+        }
+      }
+    ]
+  });  
 }
